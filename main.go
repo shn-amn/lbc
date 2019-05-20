8e00366ab6098abf18dba1f62fa83607c8e6784b
schahnie <schahnie@gmail.com>
2019-05-20 13:16:15 +0200

    Refactoring handlers: issue #3 resolved.

--- main.go
+++ main.go
@@ -9,35 +9,36 @@ import (
 	"net/http"
 )
 
-func fizzbuzzHandler(w http.ResponseWriter, r *http.Request) {
+func fizzbuzzList(r *http.Request) (int, interface {}) {
 	params, err := input.ParseParams(r.URL.Query())
 	if err == nil {
-		log.Println(params, input.RegisterStats(params))
 		f := fizzbuzz.New(params.Int1, params.Int2, params.Str1, params.Str2)
 		res := make([]string, params.Limit)
 		for i := range res {
 			res[i] = f(i + 1)
 		}
-		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(res)
-		log.Println(r.URL, 200)
-		return
+		return http.StatusOK, res
 	}
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusBadRequest)
-	json.NewEncoder(w).Encode([]string(err))
-	log.Println(r.URL, 400)
+	return http.StatusBadRequest, err
 }
 
-func statsHandler(w http.ResponseWriter, r *http.Request) {
-	mostPopular := input.MostPopular()
-	log.Println(mostPopular)
-	w.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(mostPopular)
+func stats(r *http.Request) (int, interface {}) {
+	return http.StatusOK, input.MostPopular()
 }
 
 func main() {
-	http.HandleFunc("/fizzbuzz", fizzbuzzHandler)
-	http.HandleFunc("/statistics", statsHandler)
+	http.HandleFunc("/fizzbuzz", jsonHandler(fizzbuzzList))
+	http.HandleFunc("/statistics", jsonHandler(stats))
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func jsonHandler(f func(*http.Request) (int, interface{})) (handler func(http.ResponseWriter, *http.Request)) {
+	handler = func (w http.ResponseWriter, r *http.Request) {
+		status, response := f(r)
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(status)
+		json.NewEncoder(w).Encode(response)
+		log.Println(r.URL, status)
+	}
+	return
 }
